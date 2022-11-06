960021bf6a969232eec63d46f3d3569ee1c78505
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2022-11-06 01:41:10 -0400

    Login Design

--- app/src/main/java/com/pucmm/e_commerce/MainActivity.java
+++ app/src/main/java/com/pucmm/e_commerce/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.romainpiel.shimmer.Shimmer;
+import com.romainpiel.shimmer.ShimmerTextView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ShimmerTextView shimmerTextView = findViewById(R.id.tvLoading);
+        Shimmer shimmer = new Shimmer()
+                .setDirection(Shimmer.ANIMATION_DIRECTION_LTR)
+                .setDuration(5000)
+                .setStartDelay(1000);
+        shimmer.start(shimmerTextView);
     }
 }
