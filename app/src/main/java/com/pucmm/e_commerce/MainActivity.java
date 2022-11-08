08f78e12d07b1a669199545f10e47401bc4a19eb
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2022-11-08 16:37:01 -0400

    toolbar finished

--- app/src/main/java/com/pucmm/e_commerce/MainActivity.java
+++ app/src/main/java/com/pucmm/e_commerce/MainActivity.java
@@ -5,15 +5,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
 
+import com.pucmm.e_commerce.databinding.ActivityMainBinding;
 import com.romainpiel.shimmer.Shimmer;
 import com.romainpiel.shimmer.ShimmerTextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ActivityMainBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View viewMain = binding.getRoot();
+        setContentView(viewMain);
+
+        setSupportActionBar(binding.toolbar);
+
     }
 }
