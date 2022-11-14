a16caf5d00249b7ca27d15d29c9211a931eb3db1
José Miguel Rodríguez Kelly <josephr2316@gmail.com>
2022-11-14 15:51:27 -0400

    last updated

--- app/src/main/java/com/pucmm/e_commerce/MainActivity.java
+++ app/src/main/java/com/pucmm/e_commerce/MainActivity.java
@@ -1,18 +1,25 @@
 package com.pucmm.e_commerce;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.google.android.material.navigation.NavigationView;
 import com.pucmm.e_commerce.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
+    ActionBarDrawerToggle toogle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +29,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(viewMain);
 
         setSupportActionBar(binding.toolbar);
-
+        toogle = new ActionBarDrawerToggle(this, binding.drawer,binding.toolbar, R.string.open,R.string.close);
+        binding.drawer.addDrawerListener(toogle);
+        toogle.syncState();
+        //NavigationUI.setupWithNavController( binding.navView,navController);
+        binding.navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+//                switch (item.getItemId()){
+//                    case :
+//                }
+                return false;
+            }
+        });
     }
 
     @Override
@@ -33,6 +52,20 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
         return super.onOptionsItemSelected(item);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (binding.drawer.isDrawerOpen(GravityCompat.START)){
+            binding.drawer.closeDrawer(GravityCompat.START);
+        }
+        else
+            super.onBackPressed();
     }
 }
+
+/*
+* */
